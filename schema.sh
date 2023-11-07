6ce085d1ebb4629b3dec6b0418ee07dc8ae6609a
jtaylor <jonrtaylor12@gmail.com>
2023-11-06 21:28:39 -0700

    Add petri-graph as submodule

--- schema.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-filename=".schema" # name of the file containing types list
-while read -r type; do
-    typescript-json-schema tsconfig.json "$type" -o "app/forms/$type.schema.json"
-done < "$filename"
